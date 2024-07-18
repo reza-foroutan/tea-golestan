b9e51fdb83cb88cee42c57b70e546abbaa42f0ba
reza-foroutan <rezaforoutanjs@gmail.com>
2024-07-18 19:38:52 +0330

    third commit

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -40,7 +40,195 @@ function Footer() {
           </section>
           <section className="footer-divider-border"></section>
           <section className="footer-menu">
-            <div className="footer-menu-wrapper"></div>
+            <div className="footer-menu-wrapper">
+              <div className="footer-boxes">
+                <div>
+                  <div className="box">
+                    <div>
+                      <div className="footer-image">
+                        <img
+                          src="../../public/images/footer-icon1.png"
+                          alt="چای گلستان"
+                        />
+                      </div>
+                      <div className="divider-footer"></div>
+                      <div className="title-footer">
+                        <h2>چای گلستان</h2>
+                      </div>
+                      <div className="divider-footer"></div>
+                      <div className="menu-list-footer">
+                        <div>
+                          <a href="#" target="_blank">
+                            <span> چای ممتاز هندوستان​</span>
+                          </a>
+                        </div>
+                        <div>
+                          <a href="#" target="_blank">
+                            <span> چای ممتاز ارل گری​</span>
+                          </a>
+                        </div>
+                        <div>
+                          <a href="#" target="_blank">
+                            <span> چای سيلان عطری​</span>
+                          </a>
+                        </div>
+                        <div>
+                          <a href="">
+                            <span>چای ممتاز سيلان​</span>
+                          </a>
+                        </div>
+                        <div>
+                          <a href="">
+                            <span>چای صبحانه گلستان​</span>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="box">
+                    <div>
+                      <div className="footer-image">
+                        <img
+                          src="../../public/images/footer-icon4.png"
+                          alt="تی بگ گلستان
+"
+                        />
+                      </div>
+                      <div className="divider-footer"></div>
+                      <div className="title-footer">
+                        <h2>تی بگ گلستان</h2>
+                      </div>
+                      <div className="divider-footer"></div>
+                      <div className="menu-list-footer">
+                        <div>
+                          <a href="#" target="_blank">
+                            <span> تی بگ بلک لاين</span>
+                          </a>
+                        </div>
+                        <div>
+                          <a href="#" target="_blank">
+                            <span> تی بگ ارل گری</span>
+                          </a>
+                        </div>
+                        <div>
+                          <a href="#" target="_blank">
+                            <span> دمنوش نعناع</span>
+                          </a>
+                        </div>
+                        <div>
+                          <a href="">
+                            <span>دمنوش بابونه</span>
+                          </a>
+                        </div>
+                        <div>
+                          <a href="">
+                            <span>دمنوش چای سبز</span>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="box">
+                    <div>
+                      <div className="footer-image">
+                        <img
+                          src="../../public/images/footer-icon3.png"
+                          alt="ادويه گلستان
+
+"
+                        />
+                      </div>
+                      <div className="divider-footer"></div>
+                      <div className="title-footer">
+                        <h2>ادويه گلستان</h2>
+                      </div>
+                      <div className="divider-footer"></div>
+                      <div className="menu-list-footer">
+                        <div>
+                          <a href="#" target="_blank">
+                            <span>زعفران ممتاز خراسان</span>
+                          </a>
+                        </div>
+                        <div>
+                          <a href="#" target="_blank">
+                            <span> ادویه کاری</span>
+                          </a>
+                        </div>
+                        <div>
+                          <a href="#" target="_blank">
+                            <span> ادویه ماست و خیار</span>
+                          </a>
+                        </div>
+                        <div>
+                          <a href="#">
+                            <span>فلفل سياه خالص</span>
+                          </a>
+                        </div>
+                        <div>
+                          <a href="#">
+                            <span>زردچوبه ممتاز</span>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="box">
+                    <div>
+                      <div className="footer-image">
+                        <img
+                          src="../../public/images/footer-icon2.png"
+                          alt="حبوبات گلستان"
+                        />
+                      </div>
+                      <div className="divider-footer"></div>
+                      <div className="title-footer">
+                        <h2>حبوبات گلستان</h2>
+                      </div>
+                      <div className="divider-footer"></div>
+                      <div className="menu-list-footer">
+                        <div>
+                          <a href="#" target="_blank">
+                            <span>لوبيا قرمز ممتاز</span>
+                          </a>
+                        </div>
+                        <div>
+                          <a href="#" target="_blank">
+                            <span> لوبيا چيتی گلستان</span>
+                          </a>
+                        </div>
+                        <div>
+                          <a href="#" target="_blank">
+                            <span> عدس كانادايی</span>
+                          </a>
+                        </div>
+                        <div>
+                          <a href="#">
+                            <span>نخود زودپز كرمانشاه</span>
+                          </a>
+                        </div>
+                        <div>
+                          <a href="#">
+                            <span>لوبيا سفيد گلستان</span>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+          <section className="footer-copyRight">
+            <div className="footer-copyRight-wrapper">
+              <div>
+                <div className="footer-all-right">
+                  <span>All Right Reserved Golestan Company 2022-2025</span>
+                </div>
+                <div className="footer-design">
+                  <span>Design By: Golestan Studio</span>
+                </div>
+              </div>
+            </div>
           </section>
         </div>
       </footer>
